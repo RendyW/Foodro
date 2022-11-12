2f69996a0a33c16596e27b66eb535ac9b8c5dda1
darrenxx3 <cdarren200503@gmail.com>
2022-11-12 22:31:37 +0700

    tambah animasi welcome screen,Account setup
    add anim resourcel folder too!

--- app/src/main/java/com/latutslab_00000053580/foodro_home/Account_Setup.java
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/Account_Setup.java
@@ -2,13 +2,51 @@ package com.latutslab_00000053580.foodro_home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class Account_Setup extends AppCompatActivity {
+
+    EditText inputPass;
+    CheckBox showpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setup);
+
+        inputPass = findViewById(R.id.inputPass);
+        showpassword = findViewById(R.id.showpassword);
+
+        showpassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                if(b){
+                    inputPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else {
+                    inputPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+        Button btnsignup = findViewById(R.id.signupBtn);
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Account_Setup.this, SignUp.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+    }
+    @Override
+    public void finish(){
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
