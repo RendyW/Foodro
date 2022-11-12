a05d88c958433848467639a321752648ba7a9d35
darrenxx3 <cdarren200503@gmail.com>
2022-11-12 22:31:37 +0700

    tambah animasi welcome screen,Account setup
    add anim resourcel folder too!

--- app/src/main/java/com/latutslab_00000053580/foodro_home/Welcome_Screen.java
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/Welcome_Screen.java
@@ -2,7 +2,10 @@ package com.latutslab_00000053580.foodro_home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome_Screen extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Welcome_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        Button ButtonGET = findViewById(R.id.buttonGTS);
+        ButtonGET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Welcome_Screen.this, Account_Setup.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
     }
 }
