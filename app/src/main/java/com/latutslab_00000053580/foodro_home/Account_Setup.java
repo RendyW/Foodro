8193978e5f5a4e48d9b212407baa49da199ed0c4
vincencius <vincenciuschristiano.vc@gmail.com>
2022-11-21 21:38:54 +0700

    User sqllite done

--- app/src/main/java/com/latutslab_00000053580/foodro_home/Account_Setup.java
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/Account_Setup.java
@@ -2,19 +2,25 @@ package com.latutslab_00000053580.foodro_home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import com.latutslab_00000053580.foodro.User;
+import com.latutslab_00000053580.sqlite.DbUser;
+
 public class Account_Setup extends AppCompatActivity {
 
-    EditText inputPass;
+    EditText inputPass, inputUsername;
     CheckBox showpassword;
 
     @Override
@@ -23,6 +29,7 @@ public class Account_Setup extends AppCompatActivity {
         setContentView(R.layout.activity_account_setup);
 
         inputPass = findViewById(R.id.inputPass);
+        inputUsername = findViewById(R.id.inputUsername);
         showpassword = findViewById(R.id.showpassword);
 
         showpassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -41,6 +48,24 @@ public class Account_Setup extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Account_Setup.this, SignUp.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+
+        Button btnLogin = findViewById(R.id.loginBtn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = 1;
+
+                User user = new User(inputUsername.getText().toString(), id);
+                DbUser dbUser = new DbUser(Account_Setup.this);
+                dbUser.open();
+                dbUser.addUser(user);
+
+
+                Cursor cursor = dbUser.getUser();
+                cursor.moveToFirst();
+                Log.e("User", cursor.getString(1));
             }
         });
     }
