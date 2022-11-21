f025e852a12e5bb8cbddfef7ffb523419a9f822e
vincencius <vincenciuschristiano.vc@gmail.com>
2022-11-21 21:38:19 +0700

    User selection done

--- app/src/main/java/com/latutslab_00000053580/foodro_home/Welcome_Screen.java
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/Welcome_Screen.java
@@ -3,9 +3,13 @@ package com.latutslab_00000053580.foodro_home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.latutslab_00000053580.foodro.User;
+import com.latutslab_00000053580.sqlite.DbUser;
 
 public class Welcome_Screen extends AppCompatActivity {
 
@@ -18,8 +22,17 @@ public class Welcome_Screen extends AppCompatActivity {
         ButtonGET.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Welcome_Screen.this, Account_Setup.class));
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                DbUser dbUser = new DbUser(getApplicationContext());
+//                User user = dbUser.Authenticate();
+
+                dbUser.open();
+                if(dbUser.getUser().getCount() > 0){
+                    startActivity(new Intent(Welcome_Screen.this, MainActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                } else {
+                    startActivity(new Intent(Welcome_Screen.this, Account_Setup.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
             }
         });
     }
