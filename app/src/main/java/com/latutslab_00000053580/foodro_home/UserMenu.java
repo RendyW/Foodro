5dc8b079a2edb8308e615602de7f8cea2bc3b78c
Obie <obiekrisnanto1@gmail.com>
2022-11-24 21:38:02 +0700

    Squashed commit of the following:
    commit e3fee56a801448f2af9b263ac937ea5d4794b688
    Author: RendyW <rendywt03@gmail.com>
    Date:   Thu Nov 24 17:27:43 2022 +0700
    
        Update Home_Merchant.java
    
    commit 308250694f144e3198cb77880ce5e845cf69ff2b
    Author: RendyW <rendywt03@gmail.com>
    Date:   Thu Nov 24 15:27:18 2022 +0700
    
        nerapin viewpager di order history
    
    commit 7fd89e273efc11cc0ab582cf9372f8694f130ad1
    Author: RendyW <rendywt03@gmail.com>
    Date:   Tue Nov 22 14:05:08 2022 +0700
    
        UI add menu baru di merchant

---
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/UserMenu.java
@@ -0,0 +1,41 @@
+package com.latutslab_00000053580.foodro_home;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserMenu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_menu);
+    }
+}
