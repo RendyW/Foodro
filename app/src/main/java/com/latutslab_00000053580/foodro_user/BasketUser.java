ab555d2b5bbe4831b988b3294103823a118601bb
RendyW <rendywt03@gmail.com>
2022-12-01 00:25:20 +0700

    user

---
+++ app/src/main/java/com/latutslab_00000053580/foodro_user/BasketUser.java
@@ -0,0 +1,16 @@
+package com.latutslab_00000053580.foodro_user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.latutslab_00000053580.foodro_home.R;
+
+public class BasketUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket_user);
+    }
+}
