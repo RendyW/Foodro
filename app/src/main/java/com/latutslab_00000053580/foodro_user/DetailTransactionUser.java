fbb22b528275cf46d4ba60e61a797ff851e35c4b
RendyW <rendywt03@gmail.com>
2022-12-04 23:06:25 +0700

    Detail Transaction

---
+++ app/src/main/java/com/latutslab_00000053580/foodro_user/DetailTransactionUser.java
@@ -0,0 +1,16 @@
+package com.latutslab_00000053580.foodro_user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.latutslab_00000053580.foodro_home.R;
+
+public class DetailTransactionUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_transaction_user);
+    }
+}
